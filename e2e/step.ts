3d9cc425892da369bac00fd81a3287d6452745e0
dgabka <dgabka@griddynamics.com>
2018-11-14 14:07:11 +0100

    simplified step function

--- e2e/step.ts
+++ e2e/step.ts
@@ -3,7 +3,6 @@ import {allure} from './allure-reporter';
 export function Step(name: string) {
 	return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
 		const method = descriptor.value;
-		let callable;
 
 		descriptor.value = function (...args) {
 			const countNum = (name.match(/%d/g) || []).length;
@@ -12,15 +11,11 @@ export function Step(name: string) {
 				const strings = args.filter(e => typeof e === 'string').reverse();
 				const numbers = args.filter(e => typeof e === 'number').reverse();
 				for (let i = 0; i < Math.max(countStr, countNum); i++) {
-					name = name.replace(/%s/, strings.pop());
-					name = name.replace(/%d/, numbers.pop());
+					name = name.replace(/(?<!\\)%s/, strings.pop());
+					name = name.replace(/(?<!\\)%d/, numbers.pop());
 				}
 			}
-
-			console.log(name);
-
-			callable = () => allure.step(name, () => method.apply(this, args));
-			return callable.apply(this, args);
+			return allure.step(name, () => method.apply(this, args));
 		};
 		return descriptor;
 	};
